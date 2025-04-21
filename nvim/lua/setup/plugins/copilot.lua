2fac610d91dfef15377987085d872d0b6b9693b9
Dzanija Ivan <ivan.dzanija222@gmail.com>
2025-04-21 22:35:35 +0200

    Upd

---
+++ nvim/lua/setup/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+	"github/copilot.vim",
+	lazy = false,
+	config = function()
+		vim.g.copilot_no_tab_map = true
+		vim.g.copilot_assume_mapped = true
+		vim.g.copilot_filetypes = {
+			["*"] = true,
+			["c++"] = false,
+			["cpp"] = false,
+			["c"] = false,
+		}
+	end,
+}
