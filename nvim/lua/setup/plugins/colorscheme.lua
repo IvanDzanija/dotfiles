59081dab528c6c5e3935bdb7ef5715b09664dc36
Dzanija Ivan <ivan.dzanija@tutamail.com>
2025-06-21 05:09:57 +0200

    auto-commit

--- nvim/lua/setup/plugins/colorscheme.lua
+++ nvim/lua/setup/plugins/colorscheme.lua
@@ -2,32 +2,29 @@ return {
 	--	"folke/tokyonight.nvim",
 	--	"Tsuzat/NeoSolarized.nvim",
 	--	"maxmx03/solarized.nvim",
-	--"rebelot/kanagawa.nvim",
+	--	"rebelot/kanagawa.nvim",
 	-- "savq/melange-nvim",
-	"navarasu/onedark.nvim",
-	-- "marko-cerovac/material.nvim",
+	--	"navarasu/onedark.nvim",
+	--	"marko-cerovac/material.nvim",
 	-- "blazkowolf/gruber-darker.nvim",
 	--"rose-pine/neovim",
 	--	"EdenEast/nightfox.nvim",
-	--	"ellisonleao/gruvbox.nvim",
+	"ellisonleao/gruvbox.nvim",
 	priority = 1000,
 	lazy = false,
 	transparent = true,
 	config = function()
 		vim.o.termguicolors = true
-		style = "cool"
-
 		--vim.o.background = "light"
 		--vim.cmd("colorscheme tokyonight-storm")
 		--vim.cmd("colorscheme NeoSolarized")
-		--vim.cmd("colorscheme kanagawa")
+		--	vim.cmd("colorscheme kanagawa")
 		--vim.cmd("colorscheme solarized")
-		--vim.cmd("colorscheme gruvbox")
+		vim.cmd("colorscheme gruvbox")
 		--vim.cmd("colorscheme rose-pine")
 		--vim.cmd("colorscheme dawnfox")
 		-- vim.cmd("colorscheme gruber-darker")
 		--	vim.cmd("colorscheme melange")
-		-- vim.cmd("colorscheme material")
-		vim.cmd("colorscheme onedark")
+		--vim.cmd("colorscheme material")
 	end,
 }
